3898943ddcf4c1c368fbbb1ff0fbf59487e762ed
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2019-03-13 17:14:32 +0900

    modified mysync.py

--- mysync.py
+++ mysync.py
@@ -2,6 +2,7 @@ from ftplib import FTP
 from urllib import request
 import os
 
+
 class MyFTP:
     def __init__(self, host, user='anonymous', password='jvalao@auvia.com'):
         self.host = host
@@ -38,18 +39,24 @@ class MyFTP:
             local_size = st.st_size
         except:
             pass
-        remote_size = self.get_size(name)
-        if check_size and (local_size is None or local_size != remote_size):
-            self.ftp_instance.cwd(self.remote_dir)
-            print(f'{dest_name} <- {self.host}:{self.remote_dir}/{name}')
-            if mode[0] == 'b':
-                with open(dest_name, 'wb') as f:
-                    self.ftp_instance.retrbinary(f'RETR {name}', f.write)
+        try:
+            remote_size = self.get_size(name)
+            if check_size and (local_size is None or local_size != remote_size):
+                self.ftp_instance.cwd(self.remote_dir)
+                print(f'{dest_name} <- {self.host}:{self.remote_dir}/{name}')
+                if mode[0] == 'b':
+                    with open(dest_name, 'wb') as f:
+                        self.ftp_instance.retrbinary(f'RETR {name}', f.write)
+                else:
+                    with open(dest_name, 'wt') as f:
+                        self.ftp_instance.retrlines(f'RETR {name}', f.write)
             else:
-                with open(dest_name, 'wt') as f:
-                    self.ftp_instance.retrlines(f'RETR {name}', f.write)
-        else:
-            print(f'{dest_name} up-to-date')
+                print(f'{dest_name} up-to-date')
+        except KeyboardInterrupt as e:
+            raise e
+        except:
+            print(f'{dest_name} download error')
+
 
 class MyHTTP:
     def __init__(self, base):
@@ -68,10 +75,16 @@ class MyHTTP:
             local_size = st.st_size
         except:
             pass
-        res = request.urlopen(src)
-        remote_size = int(res.getheader('Content-Length'))
-        if check_size and (local_size is None or local_size != remote_size):
-            print(f'{dest_name} <- {src}')
-            request.urlretrieve(src, dest_name)
-        else:
-            print(f'{dest_name} up-to-date')
+        try:
+            res = request.urlopen(src)
+            remote_size = int(res.getheader('Content-Length'))
+            if check_size and (local_size is None or local_size != remote_size):
+                print(f'{dest_name} <- {src}')
+                request.urlretrieve(src, dest_name)
+            else:
+                print(f'{dest_name} up-to-date')
+        except KeyboardInterrupt as e:
+            raise e
+        except:
+            print(f'{dest_name} download error')
+
