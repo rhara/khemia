d47e7ce89ef1cf085a50600d0b1a200fd08bc7dd
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2019-04-06 01:41:52 +0900

    Update khemia/util.py

--- khemia/util.py
+++ khemia/util.py
@@ -1,2 +1,18 @@
+import os
+
+
 def showattr(v):
     print(type(v), ' '.join([d for d in dir(v) if d[0] != '_']))
+
+
+def getExtension(fname):
+    dirname, bname = os.path.split(fname)
+    i = bname.rindex('.')
+    ext = bname[i+1:]
+    if ext in ['bz2', 'gz', 'xz', 'zip', 'zst']:
+        try:
+            i = bname[:i].rindex('.')
+            ext = bname[i+1:]
+        except:
+            pass
+    return ext
